122684874b1af7a784f428079e971ce87d7a194f
Rick Nijhuis <rick.r.nijhuis@gmail.com>
2021-12-03 15:18:44 +0100

    added get git version

--- TextEditor/include/version.h
+++ TextEditor/include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 0
-#define VERSION "1.0.0"
+#define VERSION_MAJOR 
+#define VERSION_MINOR 
+#define VERSION_PATCH 
+#define VERSION "v0.0.1-dirty"
